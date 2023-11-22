b7ec440d7b02e698241ddb48f30db2c4b5094969
ikepu-tp <postmaster@licsss.jp>
2023-11-22 15:59:51 +0900

    add table test

--- tests/Feature/Designer/TableDetailTest.php
+++ tests/Feature/Designer/TableDetailTest.php
@@ -3,28 +3,41 @@
 namespace Tests\Feature\Designer;
 
 use ikepu_tp\DesignerHelper\app\Models\Project;
-use ikepu_tp\DesignerHelper\app\Models\Table_outline;
+use ikepu_tp\DesignerHelper\app\Models\Table_detail;
+use ikepu_tp\DesignerHelper\app\Models\Table_setting;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class TableOutlineTest extends TestCase
+class TableDetailTest extends TestCase
 {
     use RefreshDatabase;
     use Funcs;
 
-    public $routeName = "table.outline";
+    public $routeName = "table.detail";
     public $resource = [
         "id",
-        "name",
+        "col_name",
+        "table_setting",
+        "col_digits",
+        "col_nullable",
+        "col_default",
+        "col_unique",
+        "col_primary",
+        "col_index",
         "note",
-        "timestamp",
-        "soft_delete",
     ];
     public $data = [
-        "name" => "test",
-        "note" => "test",
-        "timestamp" => true,
-        "soft_delete" => true,
+        "col_name" => "name",
+        "table_setting" => [
+            "id" => 1,
+        ],
+        "col_digits" => 100,
+        "col_nullable" => true,
+        "col_default" => "test",
+        "col_unique" => false,
+        "col_primary" => false,
+        "col_index" => true,
+        "note" => "This is a test.",
     ];
 
     public function setUp(): void
@@ -36,45 +49,46 @@ class TableOutlineTest extends TestCase
     public function getParameters(bool $item = false, array $parameters = []): array
     {
         $parameters["project"] = Project::factory()->create();
-        if ($item) $parameters["table_outline"] = Table_outline::factory()->create(["project_id" => $parameters["project"]->id]);
+        $this->data["table_setting"]["id"] = Table_setting::factory()->create(["project_id" => $parameters["project"]->id])->id;
+        if ($item) $parameters["table_detail"] = Table_detail::factory()->create(["project_id" => $parameters["project"]->id]);
         return $parameters;
     }
 
-    public function test_get_table_outlines()
+    public function test_get_table_details()
     {
         $response = $this->get(route("{$this->routeName}.index", $this->getParameters()));
         $response->assertStatus(200);
         $response->assertJsonStructure($this->getIndexResource());
     }
 
-    public function test_get_table_outline()
+    public function test_get_table_detail()
     {
         $response = $this->get(route("{$this->routeName}.show", $this->getParameters(true)));
         $response->assertStatus(200);
         $response->assertJsonStructure($this->getSuccessResource());
     }
 
-    public function test_store_table_outline()
+    public function test_store_table_detail()
     {
         $response = $this->post(route("{$this->routeName}.store", $this->getParameters()), $this->data);
         $response->assertStatus(201);
         $response->assertJsonStructure($this->getSuccessResource());
     }
 
-    public function test_update_table_outline()
+    public function test_update_table_detail()
     {
         $parameters = $this->getParameters(true);
         $response = $this->put(route("{$this->routeName}.update", $parameters), $this->data);
         $response->assertStatus(200);
         $response->assertJsonStructure($this->getSuccessResource());
-        $this->assertEquals($this->data["name"], $parameters["table_outline"]->fresh()->name);
+        $this->assertEquals($this->data["name"], $parameters["table_detail"]->fresh()->name);
     }
 
-    public function test_delete_table_outline()
+    public function test_delete_table_detail()
     {
         $parameters = $this->getParameters(true);
         $response = $this->delete(route("{$this->routeName}.destroy", $parameters));
         $response->assertStatus(204);
-        $this->assertNotNull($parameters["table_outline"]->fresh()->deleted_at);
+        $this->assertNotNull($parameters["table_detail"]->fresh()->deleted_at);
     }
 }
--- tests/Feature/Designer/TableOutlineTest.php
+++ tests/Feature/Designer/TableOutlineTest.php
@@ -3,41 +3,28 @@
 namespace Tests\Feature\Designer;
 
 use ikepu_tp\DesignerHelper\app\Models\Project;
-use ikepu_tp\DesignerHelper\app\Models\Table_detail;
-use ikepu_tp\DesignerHelper\app\Models\Table_setting;
+use ikepu_tp\DesignerHelper\app\Models\Table_outline;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class TableDetailTest extends TestCase
+class TableOutlineTest extends TestCase
 {
     use RefreshDatabase;
     use Funcs;
 
-    public $routeName = "table.detail";
+    public $routeName = "table.outline";
     public $resource = [
         "id",
-        "col_name",
-        "table_setting",
-        "col_digits",
-        "col_nullable",
-        "col_default",
-        "col_unique",
-        "col_primary",
-        "col_index",
+        "name",
         "note",
+        "timestamps",
+        "soft_delete",
     ];
     public $data = [
-        "col_name" => "name",
-        "table_setting" => [
-            "id" => 1,
-        ],
-        "col_digits" => 100,
-        "col_nullable" => true,
-        "col_default" => "test",
-        "col_unique" => false,
-        "col_primary" => false,,
-        "col_index" => true,
-        "note" => "This is a test.",
+        "name" => "test",
+        "note" => "test",
+        "timestamps" => true,
+        "soft_delete" => true,
     ];
 
     public function setUp(): void
@@ -49,46 +36,45 @@ class TableDetailTest extends TestCase
     public function getParameters(bool $item = false, array $parameters = []): array
     {
         $parameters["project"] = Project::factory()->create();
-        $this->data["table_setting"]["id"] = Table_setting::factory()->create(["project" => $parameters["project"]->id])->id;
-        if ($item) $parameters["table_detail"] = Table_detail::factory()->create(["project_id" => $parameters["project"]->id]);
+        if ($item) $parameters["table_outline"] = Table_outline::factory()->create(["project_id" => $parameters["project"]->id]);
         return $parameters;
     }
 
-    public function test_get_table_details()
+    public function test_get_table_outlines()
     {
         $response = $this->get(route("{$this->routeName}.index", $this->getParameters()));
         $response->assertStatus(200);
         $response->assertJsonStructure($this->getIndexResource());
     }
 
-    public function test_get_table_detail()
+    public function test_get_table_outline()
     {
         $response = $this->get(route("{$this->routeName}.show", $this->getParameters(true)));
         $response->assertStatus(200);
         $response->assertJsonStructure($this->getSuccessResource());
     }
 
-    public function test_store_table_detail()
+    public function test_store_table_outline()
     {
         $response = $this->post(route("{$this->routeName}.store", $this->getParameters()), $this->data);
         $response->assertStatus(201);
         $response->assertJsonStructure($this->getSuccessResource());
     }
 
-    public function test_update_table_detail()
+    public function test_update_table_outline()
     {
         $parameters = $this->getParameters(true);
         $response = $this->put(route("{$this->routeName}.update", $parameters), $this->data);
         $response->assertStatus(200);
         $response->assertJsonStructure($this->getSuccessResource());
-        $this->assertEquals($this->data["name"], $parameters["table_detail"]->fresh()->name);
+        $this->assertEquals($this->data["name"], $parameters["table_outline"]->fresh()->name);
     }
 
-    public function test_delete_table_detail()
+    public function test_delete_table_outline()
     {
         $parameters = $this->getParameters(true);
         $response = $this->delete(route("{$this->routeName}.destroy", $parameters));
         $response->assertStatus(204);
-        $this->assertNotNull($parameters["table_detail"]->fresh()->deleted_at);
+        $this->assertNotNull($parameters["table_outline"]->fresh()->deleted_at);
     }
 }
